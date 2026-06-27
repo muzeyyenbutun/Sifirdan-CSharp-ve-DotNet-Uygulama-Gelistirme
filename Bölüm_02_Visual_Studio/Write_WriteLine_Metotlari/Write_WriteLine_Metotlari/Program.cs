501b2b780d681dc95a2a642a084d4ed8fc7bc904
Müzeyyen BÜTÜN <muzeyyen.butun@tdv.org>
2026-06-27 19:19:26 +0300

    Bölüm 2 Kodları Eklendi(Tekrar)

---
+++ Bölüm_02_Visual_Studio/Write_WriteLine_Metotlari/Write_WriteLine_Metotlari/Program.cs
@@ -0,0 +1,10 @@
+﻿//Örnek 1 : Müzeyyen Bütün
+Console.Write("Müzeyyen ");
+Console.WriteLine("Bütün");
+
+
+//Örnek 2 :
+//Müzeyyen
+//Bütün
+Console.WriteLine("Müzeyyen ");
+Console.WriteLine("Bütün");
