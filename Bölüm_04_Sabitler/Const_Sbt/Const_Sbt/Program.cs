31f0a79ab8c8b9c62e466f0a2c17747224d24fac
Müzeyyen BÜTÜN <muzeyyen.butun@tdv.org>
2026-06-27 17:14:52 +0300

    Bölüm 4 Kodları Eklendi.

---
+++ Bölüm_04_Sabitler/Const_Sbt/Const_Sbt/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Pi sayısını sabit olarak tanımlayalım
+        const double pi = 3.14159;
+
+        // Değişkenleri tanımlayalım
+        double alan, cevre, yaricap;
+        string yaricapGir;
+
+        Console.WriteLine("Dairenin yarıçapını girin: ");
+        yaricapGir = Console.ReadLine();
+        yaricap = Convert.ToDouble(yaricapGir);
+        cevre = 2 * pi * yaricap;
+        alan = pi * Math.Pow(yaricap, 2);
+
+        Console.WriteLine("Dairenin alanı= {0}", alan);
+        Console.WriteLine("Dairenin çevresi= {0}", cevre);
+        Console.ReadKey();
+    }
+}
---
+++ Bölüm_04_Sabitler/Readonly_Sbt/Readonly_Sbt/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    // Pi sayısını sabit olarak tanımlayalım
+    public static readonly double pi = 3.14159;
+
+    static void Main(string[] args)
+    {
+        // Değişkenleri tanımlayalım
+        double alan, cevre, yaricap;
+        string yaricapGir;
+
+        Console.WriteLine("Dairenin yarıçapını girin: ");
+        yaricapGir = Console.ReadLine();
+        yaricap = Convert.ToDouble(yaricapGir);
+        cevre = 2 * pi * yaricap;
+        alan = pi * Math.Pow(yaricap, 2);
+
+        Console.WriteLine("Dairenin alanı= {0}", alan);
+        Console.WriteLine("Dairenin çevresi= {0}", cevre);
+        Console.ReadKey();
+    }
+}
