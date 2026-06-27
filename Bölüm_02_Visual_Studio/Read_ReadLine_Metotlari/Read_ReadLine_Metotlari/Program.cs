395a5b04927abe7f7c5f38f2c6de024444e9bb34
Müzeyyen BÜTÜN <muzeyyen.butun@tdv.org>
2026-06-27 17:12:25 +0300

    Bölüm 2 Kodları Eklendi.

---
+++ Bölüm_02_Visual_Studio/Read_ReadLine_Metotlari/Read_ReadLine_Metotlari/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("En sevdiğiniz programlama dilleri: ");
+
+string programlama_dili = Console.ReadLine();
+
+Console.WriteLine("Programlama dilleri: " + programlama_dili);
+
+
+Console.Write("En sevdiğiniz programlama dili: ");
+
+int deger = Console.Read();
+
+Console.WriteLine("Karakterin sayısal değeri: " + deger);
